a1118aa605f83a43dbc0032faff9ff71b8f2876d
jerry20091103 <jerry20091103@gmail.com>
2022-12-07 11:48:37 +0800

    Modify linting script to include training folder.

--- tests/lint.py
+++ tests/lint.py
@@ -5,11 +5,12 @@ from pylint import lint
 
 THRESHOLD = 7
 
-test_files = os.listdir('sorting_battle_gym')
+test_files = [os.path.join('sorting_battle_gym', file) for file in os.listdir('sorting_battle_gym')]
+test_files.extend([os.path.join('training', file) for file in os.listdir('training')])
 # get every .py file
 test_files = [file for file in test_files if file.endswith('.py') and not file.startswith('__init__')]
 # add the path to the files
-test_files = [os.path.join('sorting_battle_gym', file) for file in test_files]
+#test_files = [os.path.join('sorting_battle_gym', file) for file in test_files]
 
 run = lint.Run(test_files, do_exit=False) 
 score = run.linter.stats.global_note

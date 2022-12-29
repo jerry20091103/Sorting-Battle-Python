71af9313e554747469e1bbc4601129c62a1f96af
plumy2828 <plumy2828@gmail.com>
2022-12-29 14:57:26 +0800

    add random_play.py

---
+++ training/random_play.py
@@ -0,0 +1,56 @@
+'''
+This is a terminal version of the 1P random play for training
+'''
+import sys
+sys.path.append("../")
+from sorting_battle_gym.game_base import GameBase
+import random
+
+def random_playcallback(game_state):
+    # show_game_status(False, game_state['game_end'], game_state['level'], game_state['score'], game_state['grid'])
+
+    # the model gets the current state of the game
+    # the model takes action according to current state of the game
+    grid=game_state['grid']
+
+    legal_action_space = []
+    for i in range(1441):
+        action_type, action_data = trans_action_id(i)
+        if is_legal_action(action_type, action_data, grid):
+            legal_action_space.append(i)
+
+    # if there is no legal action, idle
+    if legal_action_space == []:
+        action_type = 0
+        action_data = IDLE_TIME
+        print(f'In Random, action_type: {action_type}, action_data: {action_data}')
+        return action_type, action_data
+
+    action_id = random.choice(legal_action_space)
+    # convert the action to the format that the gym can understand
+    action_type, action_data = trans_action_id(action_id)
+
+    print(f'In Random, action_type: {action_type}, action_data: {action_data}')
+    return action_type, action_data
+
+config = {
+    'player_count': 1,
+    'player_swap_delay': 40,
+    'player_select_delay': 50,
+    'player_add_new_row_delay': 10,
+    'realtime': False
+}
+
+accumulated_score = 0
+for i in range(EPISODE_NUM):
+    game_base = GameBase(config)
+    # set the callback function
+    game_base.set_callback(random_playcallback, 1)
+    # run the game
+    game_base.run_game()
+
+    print("=================================================")
+    print("EPISODE_NUM: " + str(i))
+    print("score: " + str(game_base.game_state.game_board_state.game_score_state.total_score))
+    print("=================================================")
+print("Average score: " + str(accumulated_score / EPISODE_NUM))
